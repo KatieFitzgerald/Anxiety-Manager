688f320d183206fd863179f6240c5df4de39f7de
Katie Fitzgerald <katiefitz96@gmail.com>
2018-03-05 12:17:29 +0000

    Revert "Set up preferences"
    This reverts commit 788eec135f2cc63d7db4b6f30457a56902e1d9a3.

--- AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/CreateAccountActivity.java
+++ AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/CreateAccountActivity.java
@@ -1,28 +1,22 @@
 package com.example.katiefitzgerald.anxietymanager;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
 public class CreateAccountActivity extends SensedActivity {
 
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
     EditText enterEmail;
     EditText enterPassword;
     CheckBox chosenAccount;
     Button createAccount;
-    TextView login;
 
     DatabaseReference databaseUsers;
 
@@ -37,23 +31,11 @@ public class CreateAccountActivity extends SensedActivity {
         enterPassword = findViewById(R.id.password);
         chosenAccount = findViewById(R.id.isCounsellor);
         createAccount = findViewById(R.id.createBtn);
-        login = findViewById(R.id.login);
-
-        sharedPreferences = getApplicationContext().getSharedPreferences("Create", 0);
-        editor = sharedPreferences.edit();
 
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addUser();
-            }
-        });
-
-        login.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent LoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(LoginActivity);
             }
         });
 
@@ -63,34 +45,22 @@ public class CreateAccountActivity extends SensedActivity {
         String userEmail = enterEmail.getText().toString().trim();
         String password = enterPassword.getText().toString().trim();
 
-        Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+        Intent home = new Intent(getApplicationContext(), HomeActivity.class);
 
-        if(userEmail.length() < 0){
+        if(userEmail.matches("")){
             Toast.makeText(this, "Please fill in a email address", Toast.LENGTH_LONG).show();
-            return;
         }
-        else if (password.length() < 0){
+        else if (password.matches("")){
             Toast.makeText(this, "Please fill in a password", Toast.LENGTH_LONG).show();
-            return;
         }
         else {
             if(chosenAccount.isChecked()) {
-                editor.putString("Email", userEmail);
-                editor.putString("Password", password);
-                editor.commit();
-
-                Toast.makeText(this, "New account created: " + userEmail, Toast.LENGTH_LONG).show();
-
-                startActivity(login);
-                //setContentView(R.layout.activity_home);
+                startActivity(home);
+                setContentView(R.layout.activity_home);
 
                 Toast.makeText(this, "Counsellor", Toast.LENGTH_LONG).show();
             }
             else {
-                editor.putString("Email", userEmail);
-                editor.putString("Password", password);
-                editor.commit();
-
                 String user_id = databaseUsers.push().getKey();
                 UserDao user = new UserDao(user_id, userEmail, password);
 
@@ -98,12 +68,11 @@ public class CreateAccountActivity extends SensedActivity {
 
                 Toast.makeText(this, "New account created: " + userEmail, Toast.LENGTH_LONG).show();
 
-                //home.putExtra("userId", user_id);
-                startActivity(login);
-                //setContentView(R.layout.activity_home);
+                home.putExtra("userId", user_id);
+                startActivity(home);
+                setContentView(R.layout.activity_home);
 
             }
         }
     }
-
 }
