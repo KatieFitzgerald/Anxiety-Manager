548acb24b99b3c931f42eaec04b9d380c7292d16
Katie Fitzgerald <katiefitz96@gmail.com>
2018-03-15 15:14:33 +0000

    Set up CalendarActivity layout

---
+++ AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/CalendarActivity.java
@@ -0,0 +1,20 @@
+package com.example.katiefitzgerald.anxietymanager.activities;
+
+/**
+ * Created by Katie Fitzgerald on 15/03/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.katiefitzgerald.anxietymanager.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
