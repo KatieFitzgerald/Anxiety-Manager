075c1d6bec940c813a3d89c1bb9616360991e2fe
Katie Fitzgerald <katiefitz96@gmail.com>
2018-03-17 18:40:46 +0000

    Add login authenticate from firebase

--- AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/CreateAccountActivity.java
+++ AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/CreateAccountActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -43,7 +42,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create);
 
         enterName = findViewById(R.id.enterName);
-        enterEmail = findViewById(R.id.enterEmail);
+        enterEmail = findViewById(R.id.enterUsername);
         enterPassword = findViewById(R.id.enterPassword);
         confirmPassword = findViewById(R.id.confirmPassword);
         chosenAccount = findViewById(R.id.isCounsellor);
--- AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/LoginActivity.java
+++ AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.katiefitzgerald.anxietymanager.activities;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,10 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,14 +21,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.katiefitzgerald.anxietymanager.R;
+import com.example.katiefitzgerald.anxietymanager.model.UserDao;
 import com.example.katiefitzgerald.anxietymanager.sql.DatabaseManager;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText email;
-    EditText password;
+    EditText usernameEditText;
+    EditText passwordEditText;
     Button login;
     TextView register;
+    TextView warning;
+    CoordinatorLayout coordinatorLayout;
+
+    boolean userCorrect = false;
+    boolean passwordCorrect = false;
 
     DatabaseManager databaseManager = new DatabaseManager(this);
 
@@ -36,10 +49,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.enterEmail);
-        password = findViewById(R.id.enterPassword);
+        usernameEditText = findViewById(R.id.enterUsername);
+        passwordEditText = findViewById(R.id.enterPassword);
+        warning = findViewById(R.id.passwordWarning);
         login = findViewById(R.id.loginBtn);
         register = findViewById(R.id.registerText);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,19 +75,18 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                final String enteredEmail = email.getText().toString();
-                final String enteredPassword = password.getText().toString();
+                final String enteredUsername = usernameEditText.getText().toString().trim();
+                final String enteredPassword = passwordEditText.getText().toString().trim();
 
                 login.setBackgroundResource(R.drawable.pressed);
                 login.setTextColor(Color.GRAY);
 
-                if(null!= enteredEmail && enteredEmail.length() == 0) {
+                if(null!= enteredUsername && enteredUsername.length() == 0) {
 
-                    final TextView warning = findViewById(R.id.emailWarning);
                     warning.setTextColor(Color.RED);
-                    warning.setText("Please enter an email address");
+                    warning.setText("Please enter a username");
 
-                    email.addTextChangedListener(new TextWatcher()
+                    usernameEditText.addTextChangedListener(new TextWatcher()
                     {
                         @Override
                         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -90,11 +104,10 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else if (enteredPassword.length() == 0){
 
-                    final TextView warning = findViewById(R.id.passwordWarning);
                     warning.setTextColor(Color.RED);
                     warning.setText("Please enter a password");
 
-                    password.addTextChangedListener(new TextWatcher()
+                    passwordEditText.addTextChangedListener(new TextWatcher()
                     {
                         @Override
                         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -111,22 +124,73 @@ public class LoginActivity extends AppCompatActivity {
                     });
                 }
                 else {
-                    if(databaseManager.checkUser(enteredEmail, enteredPassword)) {
 
-                        Intent home = new Intent(getApplicationContext(), HomeActivity.class);
-                        home.putExtra("EMAIL", enteredEmail);
-                        startActivity(home);
-                    }
-                    else {
-                        final TextView warning = findViewById(R.id.passwordWarning);
-                        warning.setTextColor(Color.RED);
-                        warning.setText("Login credentials are incorrect");
+                    DatabaseReference UsersDB = FirebaseDatabase.getInstance().getReference();
+                    Query userReference = UsersDB.child("user_profile").orderByChild("name").equalTo(enteredUsername);
+                    final Query passwordReference = UsersDB.child("user_profile").orderByChild("password").equalTo(enteredPassword);
 
-                        email.setText(null);
-                        password.setText(null);
-                        checkLogin();
-                    }
+                    userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
 
+                            if (dataSnapshot.getValue() != null) {
+
+                                passwordReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                        if (dataSnapshot.getValue() != null){
+
+                                            for(DataSnapshot userData: dataSnapshot.getChildren()) {
+
+                                                UserDao user = userData.getValue(UserDao.class);
+
+                                                String user_id = user.getId();
+
+                                                Intent home = new Intent(getApplicationContext(), HomeActivity.class);
+                                                home.putExtra("user_id", user_id);
+                                                startActivity(home);
+                                            }
+
+                                        }
+                                        else {
+
+                                            Snackbar snackbar = Snackbar.make(coordinatorLayout, "Incorrect login credentials", Snackbar.LENGTH_LONG);
+                                            View view = snackbar.getView();
+                                            TextView snackbarText = view.findViewById(android.support.design.R.id.snackbar_text);
+                                            snackbarText.setTextColor(Color.parseColor("#f04343"));
+                                            snackbar.show();
+
+                                            usernameEditText.setText(null);
+                                            passwordEditText.setText(null);
+
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                            else {
+
+                                Snackbar snackbar = Snackbar.make(coordinatorLayout, "Incorrect login credentials", Snackbar.LENGTH_LONG);
+                                View view = snackbar.getView();
+                                TextView snackbarText = view.findViewById(android.support.design.R.id.snackbar_text);
+                                snackbarText.setTextColor(Color.parseColor("#f04343"));
+                                snackbar.show();
+
+                                usernameEditText.setText(null);
+                                passwordEditText.setText(null);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
                 }
             }
         });
