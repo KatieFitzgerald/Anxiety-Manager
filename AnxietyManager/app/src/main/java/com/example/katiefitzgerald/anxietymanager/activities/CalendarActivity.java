fc80caa7b4ed20ea0a6f4561eef324cf261763d0
Katie Fitzgerald <katiefitz96@gmail.com>
2018-03-20 13:49:48 +0000

    Add events to user's default calendar

--- AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/CalendarActivity.java
+++ AnxietyManager/app/src/main/java/com/example/katiefitzgerald/anxietymanager/activities/CalendarActivity.java
@@ -4,8 +4,16 @@ package com.example.katiefitzgerald.anxietymanager.activities;
  * Created by Katie Fitzgerald on 15/03/2018.
  */
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -19,6 +27,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -40,6 +49,11 @@ public class CalendarActivity extends AppCompatActivity {
 
         Event ev1 = new Event(Color.RED, System.currentTimeMillis(), "Anxiety Episode");
         compactCalendar.addEvent(ev1);
+
+        Event ev2 = new Event(Color.RED, System.currentTimeMillis(), "Anxiety Episode");
+        compactCalendar.addEvent(ev2);
+
+        addToDeviceCalendar();
 
         // define a listener to receive callbacks when certain events happen.
         compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
@@ -63,5 +77,40 @@ public class CalendarActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void addToDeviceCalendar(){
+
+        ContentResolver cr = this.getContentResolver();
+        ContentValues values = new ContentValues();
+
+        values.put(CalendarContract.Events.DTSTART, System.currentTimeMillis());
+        values.put(CalendarContract.Events.TITLE, "Sensed Anxiety - Anxiety Manager");
+        values.put(CalendarContract.Events.DESCRIPTION, "Something");
+        values.put(CalendarContract.Events.EVENT_COLOR, Color.BLUE);
+
+
+        TimeZone currentTimeZone = TimeZone.getDefault();
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, currentTimeZone.getID());
+
+        // Default calendar
+        values.put(CalendarContract.Events.CALENDAR_ID, 1);
+
+        long endTime = System.currentTimeMillis() + 1800000;
+        values.put(CalendarContract.Events.DTEND, endTime);
+
+
+        //https://developer.android.com/training/permissions/requesting.html
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALENDAR)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        }
+
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
     }
 }
