c729a78c64f22813b7fe33cba7fccbafa668c9ee
Katie Fitzgerald <katiefitz96@gmail.com>
2018-04-12 23:39:35 +0100

    GSR path rename

--- GSR/GSR_Config.ino
+++ GSR/GSR_Config.ino
@@ -1,3 +1,5 @@
+// http://wiki.seeedstudio.com/Grove-GSR_Sensor/
+
 const int GSR = A0; //getting signal from pin A0
 int sensorValue = 0;
 int gsr_average = 0;
@@ -8,14 +10,17 @@ void setup(){
 
 void loop(){
   long sum = 0;
+  
   for (int i = 0; i < 10; i++) { //10 is recommened to remove glitches
+    
     sensorValue = analogRead(GSR);
     sum += sensorValue;
+    
     delay(5);
   }
 
   gsr_average = sum/10;
+  
   Serial.println(gsr_average); //Serial_Port_Reading is the value display on Serial Port(between 0~1023)
-  //Serial.println(sensorValue);
 }
 
